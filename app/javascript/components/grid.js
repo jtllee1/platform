6b9c248634656d45c7a5fd4fabf823c4b0acc17d
Leon Lee <jtllee1@gmail.com>
2020-06-17 16:52:16 +0100

    removed initial lag of movement

--- app/javascript/components/grid.js
+++ app/javascript/components/grid.js
@@ -5,30 +5,44 @@ const grid = () => {
     }
   }, false);
 
+  var keyState = {};
+
   document.addEventListener('keydown', (e) => {
+    keyState[e.keyCode] = true;
+  });
+
+  document.addEventListener('keyup', (e) => {
+    keyState[e.keyCode] = false;
+  });
+
+  function movementLoop() {
     const active = document.querySelector(".active");
     const right = document.getElementById(parseInt(active.id) + 1);
     const left = document.getElementById(parseInt(active.id) - 1);
     const down = document.getElementById(parseInt(active.id) + 10);
     const up = document.getElementById(parseInt(active.id) - 10);
 
-    if (e.keyCode === 39) {
+    if (keyState[39]) {
       right.classList.add("active");
       active.classList.remove("active");
     }
-    else if (e.keyCode === 37) {
+    else if (keyState[37]) {
       left.classList.add("active");
       active.classList.remove("active");
     }
-    else if (e.keyCode === 40) {
+    else if (keyState[40]) {
       down.classList.add("active");
       active.classList.remove("active");
     }
-    else if (e.keyCode === 38) {
+    else if (keyState[38]) {
       up.classList.add("active");
       active.classList.remove("active");
     }
-  });
+
+    setTimeout(movementLoop, 300);
+  };
+
+  movementLoop();
 };
 
 export { grid };
