e6f9f3dcec48595ff30306e3264d5f9bb19fd767
Leon Lee <jtllee1@gmail.com>
2020-06-20 20:47:39 +0100

    added collision with enemies

--- app/javascript/components/grid.js
+++ app/javascript/components/grid.js
@@ -22,19 +22,19 @@ const grid = () => {
     let down = document.getElementById(parseInt(active.id) + 20);
     let up = document.getElementById(parseInt(active.id) - 20);
 
-    if (keyState[39] && right && active.classList.contains("right-boundary") == false) {
+    if (keyState[39] && right && active.classList.contains("right-boundary") == false && right.classList.contains("enemy") == false) {
       right.classList.add("active");
       active.classList.remove("active");
     }
-    else if (keyState[37] && left && active.classList.contains("left-boundary") == false) {
+    else if (keyState[37] && left && active.classList.contains("left-boundary") == false && left.classList.contains("enemy") == false) {
       left.classList.add("active");
       active.classList.remove("active");
     }
-    else if (keyState[40] && down) {
+    else if (keyState[40] && down && down.classList.contains("enemy") == false) {
       down.classList.add("active");
       active.classList.remove("active");
     }
-    else if (keyState[38] && up) {
+    else if (keyState[38] && up && up.classList.contains("enemy") == false) {
       up.classList.add("active");
       active.classList.remove("active");
     };
