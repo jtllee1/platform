0f7ebbf3070d35c2864210f03772f3a68aeb4436
Leon Lee <jtllee1@gmail.com>
2020-06-20 20:44:01 +0100

    added enemy generation refresh

--- app/javascript/components/generate-enemies.js
+++ app/javascript/components/generate-enemies.js
@@ -1,8 +1,14 @@
 const generateEnemies = () => {
   const start = document.getElementById("start");
-  const ids = [];
+  const grids = document.querySelectorAll(".grid");
 
   start.addEventListener('click', () => {
+    const ids = [];
+
+    grids.forEach((grid) => {
+      grid.classList.remove("enemy");
+    });
+
     for (let i = 0; ids.length < 20; i++) {
       let chosenId = Math.floor(Math.random()*200) + 1;
       if (ids.includes(chosenId) == false) {
