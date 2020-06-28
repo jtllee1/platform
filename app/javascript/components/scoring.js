72943984df55b0d338381bc4b7fefebf945fc2bd
Leon Lee <jtllee1@gmail.com>
2020-06-28 20:29:56 +0100

    fixed scoring bug

--- app/javascript/components/scoring.js
+++ app/javascript/components/scoring.js
@@ -2,7 +2,7 @@ const scoring = (score, end) => {
   const scoreBoard = document.getElementById("score");
   const count = document.getElementById("timer");
 
-  if (end == false) {
+  if (end == false || count.innerText == 0) {
     scoreBoard.innerText = parseInt(scoreBoard.innerText) + score;
   }
   else {
