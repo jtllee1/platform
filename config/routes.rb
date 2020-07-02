3cabdb6ac83807b6809b9e2c74a6775da83cd05b
Leon Lee <jtllee1@gmail.com>
2020-07-02 20:02:35 +0100

    added home page and link to game page with title

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/cell_buster', to: 'pages#cell_buster'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
