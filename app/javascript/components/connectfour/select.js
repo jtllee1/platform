3c34cd99d98c08aa9aa3dbee30b07af5dceaf2a7
Leon Lee <jtllee1@gmail.com>
2020-07-12 21:08:14 +0100

    refactored code and changed hover color to match turn player

--- app/javascript/components/connectfour/select.js
+++ app/javascript/components/connectfour/select.js
@@ -14,49 +14,21 @@ const select = () => {
   slots.forEach(slot => {
     slot.addEventListener('mouseover', () => {
       slots.forEach(slot => {
-        slot.classList.remove("hover");
+        slot.classList.remove("hover-1");
+        slot.classList.remove("hover-2");
       });
 
-      if (slot.classList.contains("c-1")) {
-        columnHover(1, "hover");
-        activeColumn = 1;
-      }
-      else if (slot.classList.contains("c-2")) {
-        columnHover(2, "hover");
-        activeColumn = 2;
-      }
-      else if (slot.classList.contains("c-3")) {
-        columnHover(3, "hover");
-        activeColumn = 3;
-      }
-      else if (slot.classList.contains("c-4")) {
-        columnHover(4, "hover");
-        activeColumn = 4;
-      }
-      else if (slot.classList.contains("c-5")) {
-        columnHover(5, "hover");
-        activeColumn = 5;
-      }
-      else if (slot.classList.contains("c-6")) {
-        columnHover(6, "hover");
-        activeColumn = 6;
-      }
-      else if (slot.classList.contains("c-7")) {
-        columnHover(7, "hover");
-        activeColumn = 7;
-      }
-      else if (slot.classList.contains("c-8")) {
-        columnHover(8, "hover");
-        activeColumn = 8;
-      }
-      else if (slot.classList.contains("c-9")) {
-        columnHover(9, "hover");
-        activeColumn = 9;
-      }
-      else if (slot.classList.contains("c-10")) {
-        columnHover(10, "hover");
-        activeColumn = 10;
-      }
+      for (let step = 1; step < 11; step++) {
+        if (slot.classList.contains(`c-${step}`)) {
+          if (isOdd(turn)) {
+            columnHover(step, "hover-1");
+          }
+          else {
+            columnHover(step, "hover-2");
+          }
+          activeColumn = step;
+        };
+      };
     });
   });
 
