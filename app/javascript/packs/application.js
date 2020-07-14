3eaa38537e0d439079983ef7ffaf09b0dd296052
Leon Lee <jtllee1@gmail.com>
2020-07-14 19:51:48 +0100

    edit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -27,6 +28,7 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-// Call your functions here, e.g:
-// initSelect2();
-
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  // initSelect2();
+});
