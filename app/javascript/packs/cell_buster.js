e74b0d77f3e843f8c0b5b6ae3a566b711dd9a1ec
Leon Lee <jtllee1@gmail.com>
2020-07-16 20:39:38 +0100

    setup page for helicopter

--- app/javascript/packs/cell_buster.js
+++ app/javascript/packs/cell_buster.js
@@ -1,12 +1,10 @@
-// External imports
+// Internal imports, e.g:
+// import { initSelect2 } from '../components/init_select2';
 import { grid } from '../components/cellbuster/grid';
 import { levelAdjustor } from '../components/cellbuster/level-adjustor';
 import { laser } from '../components/cellbuster/laser';
 import { timer } from '../components/cellbuster/timer';
 import { generateEnemies } from '../components/cellbuster/generate-enemies';
-
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
 
 // Call your functions here, e.g:
 // initSelect2();
--- app/javascript/packs/helicopter.js
+++ app/javascript/packs/helicopter.js
@@ -1,7 +1,5 @@
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import { select } from '../components/connectfour/select';
 
 // Call your functions here, e.g:
 // initSelect2();
-select();
