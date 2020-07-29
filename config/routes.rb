41f43bd277aed1653b9d36d874051cf2448c021d
Leon Lee <jtllee1@gmail.com>
2020-07-29 19:24:16 +0100

    added placeholder for game

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/cell_buster', to: 'pages#cell_buster'
   get '/connect_four', to: 'pages#connect_four'
   get '/helicopter', to: 'pages#helicopter'
+  get '/game', to: 'pages#game'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
