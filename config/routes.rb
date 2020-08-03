3823686e972052a490ace663db5403c95fbc943f
Leon Lee <jtllee1@gmail.com>
2020-08-03 22:03:59 +0100

    added new files for tetris

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/connect_four', to: 'pages#connect_four'
   get '/helicopter', to: 'pages#helicopter'
   get '/caterpillar', to: 'pages#caterpillar'
+  get '/tetris', to: 'pages#tetris'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
