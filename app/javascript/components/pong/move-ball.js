a56c3d79da6f65365e10054423c5312fc9d2175f
Leon Lee <jtllee1@gmail.com>
2020-08-14 10:02:50 +0100

    corrected ball bouncing mechanic

--- app/javascript/components/pong/move-ball.js
+++ app/javascript/components/pong/move-ball.js
@@ -19,6 +19,9 @@ const moveBall = (initDirection) => {
         }
         else if (nW.classList.contains("up-boundary")) {
           direction = "SW";
+        }
+        else if (nW.classList.contains("left-boundary") && nW.classList.contains("up-boundary")) {
+          direction = "SE";
         };
       }
       else if (direction == "NE") {
@@ -32,22 +35,29 @@ const moveBall = (initDirection) => {
         }
         else if (nE.classList.contains("up-boundary")) {
           direction = "SE";
+        }
+        else if (nE.classList.contains("right-boundary") && nE.classList.contains("up-boundary")) {
+          direction = "SW";
         };
       }
       else if (direction == "SW") {
         let sW = document.getElementById(parseInt(location.id) + 29);
+        let sW2 = document.getElementById(parseInt(sW.id) + 29);
 
         sW.classList.add("ball");
         location.classList.remove("ball");
 
-        if (sW.classList.contains("left-boundary")) {
+        if (sW.classList.contains("left-boundary") && document.getElementById(parseInt(sW.id) + 30).classList.contains("active")) {
+          direction = "NE";
+        }
+        else if (sW.classList.contains("left-boundary")) {
           direction = "SE";
         }
-        else if (sW.classList.contains("active")) {
-          if (actives[0].id == sW.id) {
+        else if (sW2.classList.contains("active")) {
+          if (actives[0].id == sW2.id) {
             direction = "NW";
           }
-          else if (actives[4].id == sW.id) {
+          else if (actives[4].id == sW2.id) {
             direction = "NE";
           }
           else {
@@ -57,18 +67,22 @@ const moveBall = (initDirection) => {
       }
       else if (direction == "SE") {
         let sE = document.getElementById(parseInt(location.id) + 31);
+        let sE2 = document.getElementById(parseInt(sE.id) + 31);
 
         sE.classList.add("ball");
         location.classList.remove("ball");
 
-        if (sE.classList.contains("right-boundary")) {
+        if (sE.classList.contains("right-boundary") && document.getElementById(parseInt(sE.id) + 30).classList.contains("active")) {
+          direction = "NW";
+        }
+        else if (sE.classList.contains("right-boundary")) {
           direction = "SW";
         }
-        else if (sE.classList.contains("active")) {
-          if (actives[0].id == sE.id) {
+        else if (sE2.classList.contains("active")) {
+          if (actives[0].id == sE2.id) {
             direction = "NW";
           }
-          else if (actives[4].id == sE.id) {
+          else if (actives[4].id == sE2.id) {
             direction = "NE";
           }
           else {
@@ -77,7 +91,7 @@ const moveBall = (initDirection) => {
         };
       };
     };
-    setTimeout(ballMovement, 80);
+    setTimeout(ballMovement, 60);
   };
 
   ballMovement();
