e94a557abe63fbd4ed91f6a1199e03092bbdd141
Leon Lee <jtllee1@gmail.com>
2020-10-25 20:00:33 +0000

    added life system

--- app/javascript/components/hangman/check-letter.js
+++ app/javascript/components/hangman/check-letter.js
@@ -6,18 +6,32 @@ const checkLetter = (letter) => {
   const pokeImage = document.querySelector(".poke-image");
 
   let i = 0;
+  let loseCount = 0;
 
   hiddenLetters.forEach(hiddenLetter => {
     if (letter == chosenWord.innerText[i]) {
       hiddenLetter.innerText = letter;
 
       count.innerText = parseInt(count.innerText) + 1;
+    }
+    else {
+      loseCount++;
     };
 
-    i++
+    i++;
+
+    if (loseCount == hiddenLetters.length) {
+      let lives = document.querySelectorAll(".active");
+
+      console.log(lives[lives.length - 1]);
+
+      lives[lives.length - 1].classList.remove("active");
+    };
   });
 
-  if (count.innerText == chosenWord.innerText.length) {
+  let checkLives = document.querySelectorAll(".active");
+
+  if (count.innerText == chosenWord.innerText.length || checkLives.length == 0) {
     gameState.innerText = "Off";
     pokeImage.classList.remove("hidden");
   };
