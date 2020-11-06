fe834b72b0fd565bb2ee39e87dd772a3e3e7c818
Leon Lee <jtllee1@gmail.com>
2020-11-06 21:46:12 +0000

    added continual movement and corrected merge on non matches

--- app/javascript/components/2048/move-tile.js
+++ app/javascript/components/2048/move-tile.js
@@ -5,30 +5,32 @@ const moveTile = (direction) => {
     if (tiles[i].classList.contains("active")) {
       setTimeout( function timer() {
         if (direction == "left" && !tiles[i].classList.contains("left-boundary")) {
-          tiles[i].classList.remove("active");
-          tiles[i - 1].classList.add("active");
-
-          if (tiles[i].innerText == tiles[i - 1].innerText) {
-            tiles[i - 1].innerText = parseInt(tiles[i].innerText) + parseInt(tiles[i - 1].innerText);
-          }
-          else {
+          if (!tiles[i - 1].classList.contains("active")) {
+            tiles[i].classList.remove("active");
+            tiles[i - 1].classList.add("active");
             tiles[i - 1].innerText = tiles[i].innerText;
+            tiles[i].innerText = "";
+          }
+          else if (tiles[i].innerText == tiles[i - 1].innerText) {
+            tiles[i].classList.remove("active");
+            tiles[i - 1].classList.add("active");
+            tiles[i - 1].innerText = parseInt(tiles[i].innerText) + parseInt(tiles[i - 1].innerText);
+            tiles[i].innerText = "";
           };
-
-          tiles[i].innerText = "";
         }
         else if (direction == "up" && !tiles[i].classList.contains("up-boundary")) {
-          tiles[i].classList.remove("active");
-          tiles[i - 4].classList.add("active");
-
-          if (tiles[i].innerText == tiles[i - 4].innerText) {
-            tiles[i - 4].innerText = parseInt(tiles[i].innerText) + parseInt(tiles[i - 4].innerText);
-          }
-          else {
+          if (!tiles[i - 4].classList.contains("active")) {
+            tiles[i].classList.remove("active");
+            tiles[i - 4].classList.add("active");
             tiles[i - 4].innerText = tiles[i].innerText;
+            tiles[i].innerText = "";
+          }
+          else if (tiles[i].innerText == tiles[i - 4].innerText) {
+            tiles[i].classList.remove("active");
+            tiles[i - 4].classList.add("active");
+            tiles[i - 4].innerText = parseInt(tiles[i].innerText) + parseInt(tiles[i - 4].innerText);
+            tiles[i].innerText = "";
           };
-
-          tiles[i].innerText = "";
         };
       }, 1);
     };
@@ -38,30 +40,32 @@ const moveTile = (direction) => {
     if (tiles[i].classList.contains("active")) {
       setTimeout( function timer() {
         if (direction == "right" && !tiles[i].classList.contains("right-boundary")) {
-          tiles[i].classList.remove("active");
-          tiles[i + 1].classList.add("active");
-
-          if (tiles[i].innerText == tiles[i + 1].innerText) {
-            tiles[i + 1].innerText = parseInt(tiles[i].innerText) + parseInt(tiles[i + 1].innerText);
-          }
-          else {
+          if (!tiles[i + 1].classList.contains("active")) {
+            tiles[i].classList.remove("active");
+            tiles[i + 1].classList.add("active");
             tiles[i + 1].innerText = tiles[i].innerText;
+            tiles[i].innerText = "";
+          }
+          else if (tiles[i].innerText == tiles[i + 1].innerText) {
+            tiles[i].classList.remove("active");
+            tiles[i + 1].classList.add("active");
+            tiles[i + 1].innerText = parseInt(tiles[i].innerText) + parseInt(tiles[i + 1].innerText);
+            tiles[i].innerText = "";
           };
-
-          tiles[i].innerText = "";
         }
         else if (direction == "down" && !tiles[i].classList.contains("down-boundary")) {
-          tiles[i].classList.remove("active");
-          tiles[i + 4].classList.add("active");
-
-          if (tiles[i].innerText == tiles[i + 4].innerText) {
-            tiles[i + 4].innerText = parseInt(tiles[i].innerText) + parseInt(tiles[i + 4].innerText);
-          }
-          else {
+          if (!tiles[i + 4].classList.contains("active")) {
+            tiles[i].classList.remove("active");
+            tiles[i + 4].classList.add("active");
             tiles[i + 4].innerText = tiles[i].innerText;
+            tiles[i].innerText = "";
+          }
+          else if (tiles[i].innerText == tiles[i + 4].innerText) {
+            tiles[i].classList.remove("active");
+            tiles[i + 4].classList.add("active");
+            tiles[i + 4].innerText = parseInt(tiles[i].innerText) + parseInt(tiles[i + 4].innerText);
+            tiles[i].innerText = "";
           };
-
-          tiles[i].innerText = "";
         };
       }, 1);
     };
