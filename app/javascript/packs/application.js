82e15c7de86ebdcce685a3d7c7dd0184753f0062
Leon Lee <jtllee1@gmail.com>
2020-11-19 19:44:50 +0000

    created 3d platform

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -45,7 +45,7 @@ import { startMatch } from '../components/match3/start-match';
 import { swap } from '../components/match3/swap';
 import { startHangman } from '../components/hangman/start-hangman';
 import { start2048 } from '../components/2048/start-2048';
-import { startTest } from '../components/2048/start-test';
+import { startTest } from '../components/test/start-test';
 
 const id = document.getElementById("name").innerText;
 // Call your functions here, e.g:
