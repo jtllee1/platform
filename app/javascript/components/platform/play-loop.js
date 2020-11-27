721e7dab7d8379e70cd585319bd8dcf28703149b
Leon Lee <jtllee1@gmail.com>
2020-11-27 19:37:39 +0000

    added increasing speed

--- app/javascript/components/platform/play-loop.js
+++ app/javascript/components/platform/play-loop.js
@@ -2,6 +2,7 @@ import { actions } from '../platform/actions';
 
 const playLoop = () => {
   const gameState = document.getElementById("game-state");
+  const score = document.getElementById("score");
 
   if (gameState.innerText == "On") {
     let column = Math.floor(Math.random() * 4);
@@ -10,7 +11,15 @@ const playLoop = () => {
 
     let time = Math.floor(Math.random() * 4) + 1;
 
-    setTimeout(playLoop, time * 1000);
+    if (parseInt(score.innerText) >= 8000) {
+      setTimeout(playLoop, time * 600);
+    }
+    else if (parseInt(score.innerText) >= 4000) {
+      setTimeout(playLoop, time * 800);
+    }
+    else {
+      setTimeout(playLoop, time * 1000);
+    };
   };
 };
 
