80d68f5d3b3165fac91f8b49af2be641e2febfac
Leon Lee <jtllee1@gmail.com>
2020-12-13 19:07:01 +0000

    added obstacle blocks

--- app/javascript/components/balloonblast/start-balloon.js
+++ app/javascript/components/balloonblast/start-balloon.js
@@ -1,5 +1,11 @@
 const startBalloon = () => {
+  const grids = document.querySelectorAll(".grid");
 
+  for (let i = 0; i < 5; i++) {
+    for (let j = 12 + (22 * i); j < 21 + (22 * i); j += 2) {
+      grids[j].classList.add("block");
+    };
+  };
 };
 
 export { startBalloon };
