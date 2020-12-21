f2d75c3845e05305f161044f97a112bee2f0f74b
Leon Lee <jtllee1@gmail.com>
2020-12-21 19:32:23 +0000

    added pop animation

--- app/javascript/components/balloonblast/pop.js
+++ app/javascript/components/balloonblast/pop.js
@@ -1,4 +1,4 @@
-const pop = (num) => {
+const pop = (num, blastTiming, shockWaveTiming) => {
   const grid = document.getElementById(`${num}`);
   let right = document.getElementById(parseInt(num) + 1);
   let left = document.getElementById(parseInt(num) - 1);
@@ -8,19 +8,51 @@ const pop = (num) => {
   setTimeout( function timer() {
     if (right) {
       right.classList.remove("crate");
+
+      if (right.classList.contains("block") != true) {
+        right.classList.add("blast");
+      };
+
+      setTimeout( function timer() {
+        right.classList.remove("blast");
+      }, shockWaveTiming);
     };
     if (left) {
       left.classList.remove("crate");
+
+      if (left.classList.contains("block") != true) {
+        left.classList.add("blast");
+      };
+
+      setTimeout( function timer() {
+        left.classList.remove("blast");
+      }, shockWaveTiming);
     };
     if (up) {
       up.classList.remove("crate");
+
+      if (up.classList.contains("block") != true) {
+        up.classList.add("blast");
+      };
+
+      setTimeout( function timer() {
+        up.classList.remove("blast");
+      }, shockWaveTiming);
     };
     if (down) {
       down.classList.remove("crate");
+
+      if (down.classList.contains("block") != true) {
+        down.classList.add("blast");
+      };
+
+      setTimeout( function timer() {
+        down.classList.remove("blast");
+      }, shockWaveTiming);
     };
 
     grid.classList.remove("balloon");
-  }, 3000);
+  }, blastTiming);
 };
 
 export { pop };
