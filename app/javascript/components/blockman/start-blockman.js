b511ca68871e7b5e8b8a6685354c6294f7ab5097
Leon Lee <jtllee1@gmail.com>
2021-01-30 20:48:07 +0000

    added movement for blocks

--- app/javascript/components/blockman/start-blockman.js
+++ app/javascript/components/blockman/start-blockman.js
@@ -1,5 +1,14 @@
-const startBlockman = () => {
+import { move } from '../blockman/move';
 
+const startBlockman = () => {
+  document.addEventListener('keyup', (e) => {
+    if (e.keyCode == 37) {
+      move("left", 1);
+    }
+    else if (e.keyCode == 39) {
+      move("right", 1);
+    };
+  });
 };
 
 export { startBlockman };
