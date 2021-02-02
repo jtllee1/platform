e9826f39b24f1e866a62374b1e01be2f86d95f3b
Leon Lee <jtllee1@gmail.com>
2021-02-02 19:31:24 +0000

    added reverse laser and removal

--- app/javascript/components/blockman/shoot.js
+++ app/javascript/components/blockman/shoot.js
@@ -1,14 +1,18 @@
 const shoot = (direction) => {
   let player = "";
   let playerId = "";
+  let laserStart = "";
+  let laserCalc = "";
 
   if (direction == "forward") {
     player = "cube-player";
     playerId = 1;
+    laserStart = "l-1";
   }
   else {
     player = "cube-enemy";
     playerId = 2;
+    laserStart = "l-9";
   };
 
   const cube = document.querySelector(`.${player}`);
@@ -32,14 +36,14 @@ const shoot = (direction) => {
     column = "box";
   };
 
-  const laser = document.querySelector(`.laser-${playerId}`);
+  let laser = document.querySelector(`.laser-${playerId}`);
 
   const faces = ["front", "right", "back", "left", "bottom", "top"];
 
   faces.forEach(face => {
     let div = document.createElement('div');
     div.className = "box";
-    div.classList.add("l-1", `${face}`, `${column}`);
+    div.classList.add(`${laserStart}`, `${face}`, `${column}`);
     laser.appendChild(div);
   });
 
@@ -48,11 +52,22 @@ const shoot = (direction) => {
   for (let i = 1; i < 9; i++) {
     setTimeout( function timer() {
       laserParts.forEach(part => {
-        part.classList.remove(`l-${i}`);
-        part.classList.add(`l-${i + 1}`);
+        if (direction == "forward") {
+          part.classList.remove(`l-${i}`);
+          part.classList.add(`l-${i + 1}`);
+        }
+        else {
+          part.classList.remove(`l-${10 - i}`);
+          part.classList.add(`l-${9 - i}`);
+        };
       });
     }, i * 60 );
   };
+
+  setTimeout( function timer() {
+    laser.innerText = "";
+  }, 9 * 60);
 };
 
 export { shoot };
+
