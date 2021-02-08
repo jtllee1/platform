4af9ba546d91f1287110b2c46bf1e28b2be4069c
Leon Lee <jtllee1@gmail.com>
2021-02-08 20:04:01 +0000

    stopped enemy from hugging the sides

--- app/javascript/components/blockman/enemy-move.js
+++ app/javascript/components/blockman/enemy-move.js
@@ -2,10 +2,20 @@ import { move } from '../blockman/move';
 
 const enemyMove = () => {
   const options = ["right", "left"];
+  const enemy = document.querySelector(".cube-enemy");
+  const enemyPart = enemy.querySelector(".box");
 
-  move(options[Math.floor(Math.random() * options.length)], 2);
+  let selection = Math.floor(Math.random() * options.length);
 
-  setTimeout(enemyMove, 600);
+  if ((enemyPart.classList.contains("move-right2") && selection == 0) ||
+    (enemyPart.classList.contains("move-left2") && selection == 1)) {
+    enemyMove();
+  }
+  else {
+    move(options[selection], 2);
+
+    setTimeout(enemyMove, 600);
+  };
 };
 
 export { enemyMove };
