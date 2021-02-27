6a314124463cc27b85d0cfca3cefbde5695d7ad3
Leon Lee <jtllee1@gmail.com>
2021-02-27 19:55:18 +0000

    added movement

--- app/javascript/components/game/start-game.js
+++ app/javascript/components/game/start-game.js
@@ -7,6 +7,12 @@ const startGame = () => {
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[2]);
       this.interval = setInterval(updateGameArea, 20);
+      window.addEventListener('keydown', function (e) {
+        myGameArea.key = e.keyCode;
+      });
+      window.addEventListener('keyup', function (e) {
+        myGameArea.key = false;
+      });
     },
     clear : function() {
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -32,10 +38,29 @@ const startGame = () => {
       ctx.fillStyle = color;
       ctx.fillRect(this.x, this.y, this.width, this.height);
     };
+    this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    };
   };
 
   function updateGameArea() {
     myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    if (myGameArea.key && myGameArea.key == 37) {
+      myGamePiece.speedX = -1;
+    };
+    if (myGameArea.key && myGameArea.key == 39) {
+      myGamePiece.speedX = 1;
+    };
+    if (myGameArea.key && myGameArea.key == 38) {
+      myGamePiece.speedY = -1;
+    };
+    if (myGameArea.key && myGameArea.key == 40) {
+      myGamePiece.speedY = 1;
+    };
+    myGamePiece.newPos();
     myGamePiece.update();
   };
 };
