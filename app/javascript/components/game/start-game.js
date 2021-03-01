85b5e7dd54a6c694193766805b3eef67d90fc4da
Leon Lee <jtllee1@gmail.com>
2021-03-01 20:28:32 +0000

    added random obstacle movement and stop

--- app/javascript/components/game/start-game.js
+++ app/javascript/components/game/start-game.js
@@ -6,6 +6,7 @@ const startGame = () => {
       this.canvas.height = 270;
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[2]);
+      this.frameNo = 0;
       this.interval = setInterval(updateGameArea, 20);
       window.addEventListener('keydown', function (e) {
       // single direction movement
@@ -23,15 +24,19 @@ const startGame = () => {
     },
     clear : function() {
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+      clearInterval(this.interval);
     }
   };
 
   var myGamePiece;
+  var myObstacles = [];
 
   // variables above
 
-  myGameArea.start();
   myGamePiece = new component(30, 30, "red", 10, 120);
+  myGameArea.start();
 
   // functions below
 
@@ -49,10 +54,51 @@ const startGame = () => {
       this.x += this.speedX;
       this.y += this.speedY;
     };
+    this.crashWith = function(otherobj) {
+      var myleft = this.x;
+      var myright = this.x + (this.width);
+      var mytop = this.y;
+      var mybottom = this.y + (this.height);
+      var otherleft = otherobj.x;
+      var otherright = otherobj.x + (otherobj.width);
+      var othertop = otherobj.y;
+      var otherbottom = otherobj.y + (otherobj.height);
+      var crash = true;
+      if ((mybottom < othertop) ||
+      (mytop > otherbottom) ||
+      (myright < otherleft) ||
+      (myleft > otherright)) {
+        crash = false;
+      }
+      return crash;
+    };
   };
 
   function updateGameArea() {
+    var x, y;
+    for (let i = 0; i < myObstacles.length; i += 1) {
+      if (myGamePiece.crashWith(myObstacles[i])) {
+        myGameArea.stop();
+        return;
+      };
+    };
     myGameArea.clear();
+    myGameArea.frameNo += 1;
+    if (myGameArea.frameNo == 1 || everyinterval(150)) {
+      let x = myGameArea.canvas.width;
+      let minHeight = 20;
+      let maxHeight = 200;
+      let height = Math.floor(Math.random()*(maxHeight - minHeight + 1)+ minHeight);
+      let minGap = 50;
+      let maxGap = 200;
+      let gap = Math.floor(Math.random()*(maxGap - minGap + 1) + minGap);
+      myObstacles.push(new component(10, height, "green", x, 0));
+      myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
+    };
+    for (let i = 0; i < myObstacles.length; i += 1) {
+      myObstacles[i].x += -1;
+      myObstacles[i].update();
+    };
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
   // single direction movement
@@ -83,6 +129,12 @@ const startGame = () => {
     };
     myGamePiece.newPos();
     myGamePiece.update();
+    myObstacle.update();
+  };
+
+  function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    return false;
   };
 };
 
